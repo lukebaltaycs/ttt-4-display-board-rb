18720cf97bf7eb1ae58d36bdc0651e387753ab6c
Luke Baltay <luke.baltay@yale.edu>
2022-05-26 21:53:57 -0400

    commit tree 9da1a74c25753c9f552c6a9cd584eb1f99e8ffa3

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,16 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board (board)
+    dash = "-----------"
+    row_one = " #{board[0]} | #{board[1]} | #{board[2]} "
+    row_two = " #{board[3]} | #{board[4]} | #{board[5]} "
+    row_three = " #{board[6]} | #{board[7]} | #{board[8]} "
+
+    puts row_one
+    puts dash
+    puts row_two
+    puts dash
+    puts row_three
+
+end
